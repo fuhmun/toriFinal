f25ab3a3165fb2f25e8ccc2a1cc9a75ab374716c
fahadmunawar <fahadshaikh006@gmail.com>
2024-06-02 14:08:32 -0400

    Initial Commit

---
+++ toriFinal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  toriFinal
+//
+//  Created by Fahad Munawar on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ toriFinal/toriFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  toriFinalApp.swift
+//  toriFinal
+//
+//  Created by Fahad Munawar on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct toriFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

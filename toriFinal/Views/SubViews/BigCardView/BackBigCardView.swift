a90b75971aa5d0077bc07ac0ef7b3ecf46e49c1a
Alex Wash <Washingtonalex6901@gmail.com>
2024-06-06 08:48:46 -0400

    small changes

--- toriFinal/Views/SubViews/BigCardView/BackBigCardView.swift
+++ toriFinal/Views/SubViews/BigCardView/BackBigCardView.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 import SwiftData
+import SDWebImageSwiftUI
 
 struct BackBigCardView: View {
     
@@ -17,7 +18,41 @@ struct BackBigCardView: View {
     
     var body: some View {
         VStack {
-            Text("Details")
+            if let imageUrl = activityCards.image_url {
+                AnimatedImage(url: URL(string: imageUrl))
+                    .resizable()
+                    .frame(width: geoProx.size.width/1.25, height: geoProx.size.height/1.25)
+                    .overlay(
+                        LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: .top, endPoint: .center)
+                    )
+                    .overlay(
+                        VStack {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    if let name = activityCards.name {
+                                        Text(name)
+                                            .font(.title3)
+                                    }
+                                    if let city = activityCards.location?.city, let state = activityCards.location?.state {
+                                        Text("\(city), \(state)")
+                                    }
+                                    if let distance = activityCards.distance {
+                                        let miles = distance * 0.000621371
+                                        Text(String(format: "%.2f mi", miles))
+                                    }
+                                }
+                                .foregroundStyle(.white)
+                                Spacer()
+                            }
+                            .padding(geoProx.size.height/30)
+                            RoundedRectangle(cornerRadius: 25.0)
+                                .fill(.white.opacity(0.75))
+                                .frame(width: geoProx.size.width*0.7, height: geoProx.size.height*0.5)
+                                
+                        }
+                        ,alignment: .topLeading
+                    )
+            }
         }
         .rotation3DEffect(Angle(degrees: 180), axis: (x: 0.0, y: 1.0, z: 0.0))
     }
